1f237aa51c50d736b577b4062b7461ed48a5e80d
qianxuege <qianxuege15@gmail.com>
2025-10-07 10:00:09 -0400

    fix: changed name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Rohan";
+    return "qge";
   }
 
   if (query.toLowerCase().includes("what is your andrew id?")) {
